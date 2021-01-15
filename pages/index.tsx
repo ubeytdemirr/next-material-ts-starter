3a04290c5b3e55aa26491deedd0d380addcdc01f
Ubeyt Demir <190541075@firat.edu.tr>
2021-01-15 21:48:36 +0300

    adds initial files

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import * as React from "react";
+
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js Typescript Starter
+        </Typography>
+      </Box>
+    </Container>
+  );
+}
